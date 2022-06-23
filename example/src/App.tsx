c1a16553d3c0a369a36955d6db71514462a6e5da
xuyunshi <405029644@qq.com>
2022-06-23 14:06:02 +0800

    Update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import {  StyleSheet, View } from 'react-native';
-import { Room, WhiteboardView, WhiteboardReplayView, RoomPlayer, SDK, SDKConfig, RoomCallbackHandler, SDKCallbackHandler } from '../../lib/module';
+import {  Button, StyleSheet, View } from 'react-native';
+import { WhiteboardView, WhiteboardReplayView } from '../../lib/module';
+import type { Room, RoomPlayer, SDK, SDKConfig, RoomCallbackHandler, SDKCallbackHandler } from '../../lib/typescript/index';
 import { Panel } from './Panel';
 import { createWBStore } from './WBStore';
 import { appIdentifier, replayToken, replayUuid, roomToken, uid, userPayload, uuid } from './roomConst';
@@ -8,6 +9,7 @@ import type { ReplayRoomParams } from 'white-web-sdk';
 import type { ReplayCallbackHandler } from 'react-native-whiteboard';
 
 let room: Room | undefined;
+let roomPlayer: RoomPlayer | undefined;
 let sdk: SDK | undefined;
 
 const sdkParams: SDKConfig = {log: true, userCursor: true, __platform: 'rn', appIdentifier, useMultiViews: true};
@@ -21,18 +23,20 @@ const MemoWhiteboardView = React.memo(WhiteboardView);
 const MemoWhiteboardReplayView = React.memo(WhiteboardReplayView);
 
 const roomCallbacks: Partial<RoomCallbackHandler> = {
-  onPhaseChanged: e=>console.log('state changed: ', e),
+  onPhaseChanged: e=>console.log('room state changed: ', e),
 }
 
 const sdkCallbacks: Partial<SDKCallbackHandler> = {
-  onSetupFail: error=>console.log('setup fail: ', error)
+  onSetupFail: error=>console.log('sdk setup fail: ', error)
 }
 
 const replayCallbacks: Partial<ReplayCallbackHandler> = {
-  onPhaseChanged: e=>console.log('player phas: ', e)
+  onPhaseChanged: e=>console.log('player phase: ', e)
 }
 
 export default function App() {
+  const [showRoom, setShowRoom] = React.useState<Boolean>(false);
+  const [showPlayer, setShowPlayer] = React.useState<Boolean>(false);
   const [showPanel, setShowPanel] = React.useState<Boolean>(false);
 
   const joinRoomCallback = React.useCallback((aRoom, aSdk, error) => {
@@ -46,41 +50,72 @@ export default function App() {
   }, []);
 
   const replayCallback = React.useCallback((player?: RoomPlayer, sdk?: SDK, error?: Error) => {
+    roomPlayer = player;
     player.play();
   }, []);
 
   return (
     <View style={styles.whiteboardContainer}>
-      <MemoWhiteboardView
-        style={styles.whiteboard}
-        sdkConfig={sdkParams}
-        roomConfig={roomParams}
-        joinRoomCallback={joinRoomCallback}
-        roomCallbacks={roomCallbacks}
-        sdkCallbacks={sdkCallbacks}
-      />
-      {
-        showPanel && <Panel style={styles.panel} store={createWBStore({room, sdk})} />
+      {(!showRoom && !showPlayer) &&
+        <View style={styles.buttonContainer}>
+          <Button title='Live Room' onPress={() => setShowRoom(!showRoom)}></Button>
+          <Button title='Player' onPress={() => setShowPlayer(!showPlayer)}></Button>
+        </View>
       }
-      {/* <MemoWhiteboardReplayView 
-      replayConfig={replayrParams} 
-      sdkConfig={sdkParams}
-      style={styles.whiteboard}
-      replayRoomCallback={replayCallback}
-      replayCallbacks={replayCallbacks}
-      >
-      </MemoWhiteboardReplayView> */}
+            
+      { showRoom && <MemoWhiteboardView
+            style={styles.whiteboard}
+            sdkConfig={sdkParams}
+            roomConfig={roomParams}
+            joinRoomCallback={joinRoomCallback}
+            roomCallbacks={roomCallbacks}
+            sdkCallbacks={sdkCallbacks}
+          />
+      }
+      { showPanel && <Panel style={styles.panel} store={createWBStore({ room, sdk })} /> }
+      { showPanel && <Button title='Leave Room' onPress={async ()=> {
+        try {
+          await room.disconnect();
+          setShowPanel(false);
+          setShowRoom(false);
+        } catch(error) {
+          console.log(error)
+        }
+      }}/>}
+
+      {showPlayer && <MemoWhiteboardReplayView
+        replayConfig={replayrParams}
+        sdkConfig={sdkParams}
+        style={styles.whiteboard}
+        replayRoomCallback={replayCallback}
+        replayCallbacks={replayCallbacks}
+      />
+      }
+      { showPlayer && <Button title='Leave Player' onPress={async ()=> {
+        try {
+          roomPlayer.stop();
+          setTimeout(() => {
+            setShowPlayer(false);
+          }, 0);
+        } catch(error) {
+          console.log(error)
+        }
+      }}/>}
       
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1, 
+    justifyContent: 'center'
+  },
   whiteboardContainer: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    backgroundColor: 'yellow',
+    backgroundColor: 'black',
   },
   whiteboard: {
     aspectRatio: 16.0/9.0,
